59af91df03acb5207b1829700750f82893518c1b
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2020-08-16 23:34:11 +0300

    Get elements into variables

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// DOM
+const wordEl = document.getElementById('word');
+const wrongLetterEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['javascript', 'framework', 'prototype', 'closure', 'hashtable', 'encapsulation'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+console.log(selectedWord);
